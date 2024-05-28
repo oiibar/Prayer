7c485d39c1a07c9c6596ee7fafe7d04601c1466a
oiibar <95119839+oiibar@users.noreply.github.com>
2024-05-28 14:30:04 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,10 +6,16 @@ import cityRoutes from "./routes/cities.routes.js";
 dotenv.config();
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://prayer-client.onrender.com"],
+    credentials: true,
+    optionsSuccessStatus: 200,
+  })
+);
 
 app.use("/api", cityRoutes);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Listening on port ${process.env.PORT}`);
+app.listen(5000, () => {
+  console.log(`Listening on port 5000`);
 });

e1f0b834c278af845f948c70bc1372a6ce801f4f
oiibar <95119839+oiibar@users.noreply.github.com>
2024-05-28 14:38:06 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["https://prayer-client.onrender.com"],
+    origin: ["https://prayer-cli.onrender.com"],
     credentials: true,
     optionsSuccessStatus: 200,
   })
